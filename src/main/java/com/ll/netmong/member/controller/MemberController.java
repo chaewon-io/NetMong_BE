e42d9fd477dfd76ceb069fff9ea9e24e772c9148
kkambbak <yunsung9841@gmail.com>
2023-11-19 16:07:58 +0900

    init

---
+++ src/main/java/com/ll/netmong/member/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.ll.netmong.member.controller;
+
+public class MemberController {
+}
---
+++ src/test/java/com/ll/netmong/NetMongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.netmong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetMongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0d4673148469b025d7bb68d734602d4f8a320d8f
jhnyuk <debussysuh@gmail.com>
2023-11-22 14:56:40 +0900

    Feat: 포스트 댓글 리포지터리 추가

---
+++ src/main/java/com/ll/netmong/domain/postComment/repository/PostCommentRepository.java
@@ -0,0 +1,17 @@
+package com.ll.netmong.domain.postComment.repository;
+
+import com.ll.netmong.domain.postComment.entity.PostComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+
+    @Transactional(readOnly = true)
+    @Modifying
+    @Query(value = "ALTER TABLE postComment AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
+}

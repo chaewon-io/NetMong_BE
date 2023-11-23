2f4e9816fa099251a03382d752aea30e5200ff72
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-23 15:00:34 +0900

    Feat: 상품 전체 조회 카테고리 필드 추가

--- src/main/java/com/ll/netmong/domain/product/dto/response/ViewAllResponse.java
+++ src/main/java/com/ll/netmong/domain/product/dto/response/ViewAllResponse.java
@@ -1,6 +1,7 @@
 package com.ll.netmong.domain.product.dto.response;
 
 import com.ll.netmong.domain.product.entity.Product;
+import com.ll.netmong.domain.product.util.Category;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +11,12 @@ public class ViewAllResponse {
     private String productName;
     private String price;
     private String content;
+    private Category category;
 
     public ViewAllResponse(Product product) {
         this.productName = product.getProductName();
         this.price = product.getPrice();
         this.content = product.getContent();
+        this.category = product.getCategory();
     }
 }

3a2c1ef201e177cb33def5278579409876b94cd6
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-24 17:24:41 +0900

    Feat: 등록된 상품 수정을 위한 DTO 클래스 추가

---
+++ src/main/java/com/ll/netmong/domain/product/dto/request/UpdateRequest.java
@@ -0,0 +1,14 @@
+package com.ll.netmong.domain.product.dto.request;
+
+import com.ll.netmong.domain.product.util.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateRequest {
+    private String productName;
+    private String price;
+    private String content;
+    private Category category;
+}

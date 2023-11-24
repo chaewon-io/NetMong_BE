03110552d542ea81bda4591b57949db286353163
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-24 17:28:42 +0900

    Feat: 상품 수정, 논리 삭제 기능 추가

--- src/main/java/com/ll/netmong/domain/product/controller/ProductController.java
+++ src/main/java/com/ll/netmong/domain/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ll.netmong.domain.product.controller;
 
 import com.ll.netmong.common.RsData;
 import com.ll.netmong.domain.product.dto.request.CreateRequest;
+import com.ll.netmong.domain.product.dto.request.UpdateRequest;
 import com.ll.netmong.domain.product.dto.response.ViewAllResponse;
 import com.ll.netmong.domain.product.dto.response.ViewSingleResponse;
 import com.ll.netmong.domain.product.service.ProductService;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,6 +20,8 @@ import java.util.List;
 public class ProductController {
     private static final String FIND_SUCCESS_PRODUCT = "상품 조회가 되었습니다.";
     private static final String POST_SUCCESS_PRODUCT = "상품이 등록 되었습니다.";
+    private static final String MODIFY_SUCCESS_PRODUCT = "상품이 수정 되었습니다.";
+    private static final String DELETE_SUCCESS_PRODUCT = "상품이 삭제 되었습니다.";
     private final ProductService productService;
 
     @GetMapping
@@ -37,8 +41,21 @@ public class ProductController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public RsData createProduct(@ModelAttribute CreateRequest createRequest,
-                                @ModelAttribute("images") MultipartFile[] images) {
+                                @ModelAttribute("images") MultipartFile[] images) throws IOException {
         productService.createProductWithImage(createRequest, images);
         return RsData.of("S-1", POST_SUCCESS_PRODUCT, "create");
+    }
+
+    @PutMapping("/{id}")
+    public RsData updateProduct(@PathVariable(name = "id") Long productId,
+                                @ModelAttribute UpdateRequest updateRequest) {
+        productService.updateProduct(productId, updateRequest);
+        return RsData.of(MODIFY_SUCCESS_PRODUCT, "modify");
+    }
+
+    @DeleteMapping("/{id}")
+    public RsData softDeleteProduct(@PathVariable(name = "id") Long productId) {
+        productService.softDeleteProduct(productId);
+        return RsData.of(DELETE_SUCCESS_PRODUCT, "delete");
     }
 }

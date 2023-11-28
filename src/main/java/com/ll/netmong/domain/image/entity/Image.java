e4c1e2329e26add332757ba082d3d0256dde10b9
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-28 10:39:40 +0900

    Feat: 이미지 논리삭제 기능을 위한 deleted_at 컬럼 추가

--- src/main/java/com/ll/netmong/domain/image/entity/Image.java
+++ src/main/java/com/ll/netmong/domain/image/entity/Image.java
@@ -7,12 +7,17 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.SQLDelete;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter
 @Table(name = "image")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @SuperBuilder(toBuilder = true)
+@SQLDelete(sql = "UPDATE image SET deleted_at = CURRENT_TIMESTAMP where id = ?")
 public class Image extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -21,6 +26,9 @@ public class Image extends BaseEntity {
 
     @Column(name = "image_url")
     private String imageUrl;
+
+    @Column(name = "deleted_at")
+    private String deleteDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
 
     public Image(final String imageUrl) {
         this.imageUrl = imageUrl;

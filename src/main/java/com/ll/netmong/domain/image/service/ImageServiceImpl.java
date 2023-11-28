4ce05bcea07457846e73180c5d9d3afa89e05866
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-28 10:58:51 +0900

    Feat: 이미지 업로드 기능 추가

---
+++ src/main/java/com/ll/netmong/domain/image/service/ImageServiceImpl.java
@@ -0,0 +1,45 @@
+package com.ll.netmong.domain.image.service;
+
+import com.ll.netmong.domain.image.entity.Image;
+import com.ll.netmong.domain.image.repository.ImageRepository;
+import com.ll.netmong.domain.product.entity.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+    private final ImageRepository imageRepository;
+
+    @Value("${spring.servlet.multipart.location}")
+    private String productImagePath;
+
+    public void uploadImage(Product product, MultipartFile[] images) throws IOException {
+        for (MultipartFile image : images) {
+            String imageLocation = productImagePath;
+            String imageName = image.getOriginalFilename();
+            String imagePath = imageLocation + imageName;
+
+            validateCreateDirectory(imageLocation);
+            validateTransferImage(imagePath, image);
+
+            Image productImage = Product.createProductImage(imagePath);
+            product.addProductImage(productImage);
+            imageRepository.save(productImage);
+        }
+    }
+
+    private void validateCreateDirectory(String imageLocation) throws IOException {
+        Files.createDirectories(Path.of(imageLocation));
+    }
+
+    private void validateTransferImage(String imagePath, MultipartFile image) throws IOException {
+        image.transferTo(Path.of(imagePath));
+    }
+}

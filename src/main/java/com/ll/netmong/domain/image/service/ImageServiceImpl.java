187d20a9b96cffe10841b63a363308c4b9939397
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-28 13:11:00 +0900

    Feat: 트랜잭션 어노테이션 추가

--- src/main/java/com/ll/netmong/domain/image/service/ImageServiceImpl.java
+++ src/main/java/com/ll/netmong/domain/image/service/ImageServiceImpl.java
@@ -6,6 +6,7 @@ import com.ll.netmong.domain.product.entity.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -20,6 +21,7 @@ public class ImageServiceImpl implements ImageService {
     @Value("${spring.servlet.multipart.location}")
     private String productImagePath;
 
+    @Transactional
     public void uploadImage(Product product, MultipartFile[] images) throws IOException {
         for (MultipartFile image : images) {
             String imageLocation = productImagePath;

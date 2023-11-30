fb52983e34f1121953f0c574729ed4a019a582c8
shinjaewon99 <tlswodnjs99@naver.com>
2023-11-30 13:28:47 +0900

    Feat: 상품 등록, 수정에 대한 요청 DTO 제약조건 추가

--- src/main/java/com/ll/netmong/domain/product/dto/request/CreateRequest.java
+++ src/main/java/com/ll/netmong/domain/product/dto/request/CreateRequest.java
@@ -1,15 +1,22 @@
 package com.ll.netmong.domain.product.dto.request;
 
 import com.ll.netmong.domain.product.util.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 public class CreateRequest {
+    @NotBlank(message = "상품 이름은 필수 입니다.")
     private String productName;
+    @NotBlank(message = "상품 가격은 필수 입니다.")
     private String price;
+    @NotBlank(message = "상품 설명은 필수 입니다.")
     private String content;
+    @NotNull(message = "상품 카테고리는 필수 입니다.")
     private Category category;
-    private String imageUrl;
+    private MultipartFile images;
 }

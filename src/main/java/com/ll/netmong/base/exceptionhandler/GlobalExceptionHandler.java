fc65b473c62593ba4f592c3e3097146181519e02
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-01 14:15:38 +0900

    Feat: MethodArgumentNotValidException 핸들링 기능 추가

--- src/main/java/com/ll/netmong/base/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/ll/netmong/base/exceptionhandler/GlobalExceptionHandler.java
@@ -6,15 +6,19 @@ import com.ll.netmong.domain.member.exception.NotMatchPasswordException;
 import com.ll.netmong.domain.product.dto.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.security.auth.login.AccountNotFoundException;
 import java.io.IOException;
+import java.util.Optional;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final String INVALID_PRODUCT_REQUEST = "유효하지 않은 요청 입니다.";
 
     @ExceptionHandler(AccountNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -44,10 +48,14 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
- 
-//    @ExceptionHandler(Exception.class)
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    public RsData handleException(Exception e) {
-//        return RsData.failOf("Unexpected error");
-//    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleInvalidRequest(MethodArgumentNotValidException exception) {
+        String message = Optional.ofNullable(exception.getBindingResult().getFieldError())
+                .map(FieldError::getDefaultMessage)
+                .orElseThrow(() -> new RuntimeException(INVALID_PRODUCT_REQUEST));
+
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
 }

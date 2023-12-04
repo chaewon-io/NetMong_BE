9b76e800245457f1f1d2f9f10a14564ecb4acfe2
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-04 23:13:36 +0900

    Feat: 장바구니에 담을 상품 갯수 요청 DTO 추가

---
+++ src/main/java/com/ll/netmong/domain/productCart/dto/request/ProductCountRequest.java
@@ -0,0 +1,10 @@
+package com.ll.netmong.domain.productCart.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductCountRequest {
+    private Integer count;
+}

c392e15885506a71ca0a72f10d33b857fe315ae8
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-05 09:59:50 +0900

    Feat: 상품 정보가 담긴 ItemCart 레포지토리 추가

--- src/main/java/com/ll/netmong/domain/productCart/repository/ItemCartRepository.java
+++ src/main/java/com/ll/netmong/domain/productCart/repository/ItemCartRepository.java
@@ -4,4 +4,5 @@ import com.ll.netmong.domain.productCart.entity.ItemCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemCartRepository extends JpaRepository<ItemCart, Long> {
+    ItemCart findByCartIdAndProductId(Long cartId, Long productId);
 }

cfc3e00e24027909b79ff44a06a0769a8e41dd34
jhnyuk <debussysuh@gmail.com>
2023-12-05 09:59:42 +0900

    Feat: 신고 유형 목록 반환

--- src/main/java/com/ll/netmong/domain/reportPost/controller/ReportPostController.java
+++ src/main/java/com/ll/netmong/domain/reportPost/controller/ReportPostController.java
@@ -4,10 +4,15 @@ import com.ll.netmong.common.RsData;
 import com.ll.netmong.domain.reportPost.dto.request.ReportPostRequest;
 import com.ll.netmong.domain.reportPost.dto.response.ReportPostResponse;
 import com.ll.netmong.domain.reportPost.service.ReportPostService;
+import com.ll.netmong.domain.reportPost.util.ReportType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +25,13 @@ public class ReportPostController {
     public RsData<ReportPostResponse> reportPost(@PathVariable Long postId, @RequestBody ReportPostRequest request, @AuthenticationPrincipal UserDetails userDetails) {
         ReportPostResponse response = service.reportPost(postId, request, userDetails);
         return RsData.successOf(response);
+    }
+
+    @GetMapping("/types")
+    public RsData<List<String>> getReportTypes() {
+        List<String> reportTypes = Arrays.stream(ReportType.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return RsData.successOf(reportTypes);
     }
 }

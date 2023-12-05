3fd30af8a7946a9cb8dfb5165a24c0c251ea0816
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-05 10:27:14 +0900

    Feat: 장바구니 레포지토리 추가

--- src/main/java/com/ll/netmong/domain/productCart/repository/CartRepository.java
+++ src/main/java/com/ll/netmong/domain/productCart/repository/CartRepository.java
@@ -3,5 +3,8 @@ package com.ll.netmong.domain.productCart.repository;
 import com.ll.netmong.domain.productCart.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByMemberUsername(String findMemberName);
 }

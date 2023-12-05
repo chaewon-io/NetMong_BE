bd9eecf30969e8012de674e23c25c6ab6d4843b6
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-05 15:34:45 +0900

    Refactor: 장바구니 조회 기능 리팩토링

--- src/main/java/com/ll/netmong/domain/cart/controller/CartController.java
+++ src/main/java/com/ll/netmong/domain/cart/controller/CartController.java
@@ -1,8 +1,9 @@
-package com.ll.netmong.domain.productCart.controller;
+package com.ll.netmong.domain.cart.controller;
 
 import com.ll.netmong.common.RsData;
-import com.ll.netmong.domain.productCart.dto.request.ProductCountRequest;
-import com.ll.netmong.domain.productCart.service.CartService;
+import com.ll.netmong.domain.cart.dto.request.ProductCountRequest;
+import com.ll.netmong.domain.cart.itemCart.service.ItemCartService;
+import com.ll.netmong.domain.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,17 +15,18 @@ import org.springframework.web.bind.annotation.*;
 public class CartController {
     private static final String CART_SUCCESS_PRODUCT = "장바구니에 상품이 등록 되었습니다.";
     private final CartService cartService;
+    private final ItemCartService itemCartService;
 
     @GetMapping
     public RsData readProductCartByUser(@AuthenticationPrincipal UserDetails userDetails) {
         String findMemberName = userDetails.getUsername();
-        return RsData.successOf(cartService.readMemberCartByUser(findMemberName));
+        return RsData.successOf(itemCartService.readMemberCartByUser(findMemberName));
     }
 
     @PostMapping("{productId}")
     public RsData addMyCart(@AuthenticationPrincipal UserDetails userDetails,
-                          @PathVariable(name = "productId") Long productId,
-                          @RequestBody ProductCountRequest productCountRequest) {
+                            @PathVariable(name = "productId") Long productId,
+                            @RequestBody ProductCountRequest productCountRequest) {
         String findMemberName = userDetails.getUsername();
         cartService.addProductByCart(findMemberName, productId, productCountRequest);
 

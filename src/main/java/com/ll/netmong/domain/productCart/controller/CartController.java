0a30da966cc49be1f64b2f0d2646a4f25f8a3ee0
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-05 10:21:29 +0900

    Feat: 장바구니 상품 등록 기능, 장바구니 조회 기능 추가

--- src/main/java/com/ll/netmong/domain/productCart/controller/CartController.java
+++ src/main/java/com/ll/netmong/domain/productCart/controller/CartController.java
@@ -1,26 +1,33 @@
 package com.ll.netmong.domain.productCart.controller;
 
-import com.ll.netmong.domain.member.entity.Member;
-import com.ll.netmong.domain.productCart.entity.Cart;
+import com.ll.netmong.common.RsData;
+import com.ll.netmong.domain.productCart.dto.request.ProductCountRequest;
 import com.ll.netmong.domain.productCart.service.CartService;
-import com.ll.netmong.domain.productCart.service.CartServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/products/cart")
 public class CartController {
+    private static final String CART_SUCCESS_PRODUCT = "장바구니에 상품이 등록 되었습니다.";
     private final CartService cartService;
 
     @GetMapping
-    public void readProductCartByUser() {
-//        cartService.viewCartResponse();
+    public RsData readProductCartByUser(@AuthenticationPrincipal UserDetails userDetails) {
+        String findMemberName = userDetails.getUsername();
+        return RsData.successOf(cartService.readMemberCartByUser(findMemberName));
     }
 
     @PostMapping("{productId}")
-    public void addItemByCart(@PathVariable(name = "productId") String productId, Member foundMember){
+    public RsData addMyCart(@AuthenticationPrincipal UserDetails userDetails,
+                          @PathVariable(name = "productId") Long productId,
+                          @RequestBody ProductCountRequest productCountRequest) {
+        String findMemberName = userDetails.getUsername();
+        cartService.addProductByCart(findMemberName, productId, productCountRequest);
 
-        Cart.createCart(foundMember);
+        return RsData.of("S-1", CART_SUCCESS_PRODUCT, "create");
     }
 }
--- src/main/java/com/ll/netmong/domain/productCart/service/CartService.java
+++ src/main/java/com/ll/netmong/domain/productCart/service/CartService.java
@@ -1,11 +1,15 @@
 package com.ll.netmong.domain.productCart.service;
 
 import com.ll.netmong.domain.member.entity.Member;
+import com.ll.netmong.domain.productCart.dto.request.ProductCountRequest;
 import com.ll.netmong.domain.productCart.dto.response.ViewCartResponse;
+
+import java.util.List;
 
 public interface CartService {
     void createCart(Member member);
-//    void addItemByCart()
 
-    ViewCartResponse viewCartResponse(Long productId);
+    void addProductByCart(String findByMemberName, Long productId, ProductCountRequest productCountRequest);
+
+    List<ViewCartResponse> readMemberCartByUser(String findMemberName);
 }

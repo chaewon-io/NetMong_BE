86d5d456dfa3d2430c4742eaa43ae43ccbde7b5b
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-07 10:33:13 +0900

    Feat: 상품 카테고리, 이름 찾기 findBy 추가

--- src/main/java/com/ll/netmong/domain/product/repository/ProductRepository.java
+++ src/main/java/com/ll/netmong/domain/product/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package com.ll.netmong.domain.product.repository;
 
 import com.ll.netmong.domain.product.entity.Product;
+import com.ll.netmong.domain.product.util.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(Category category);
+
+    List<Product> findByProductName(String productName);
 }

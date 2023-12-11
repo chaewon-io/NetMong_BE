d45b03b44c38a29992e9c795332c4af70aea6d44
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-11 17:02:28 +0900

    Feat: 비관적 락을 위한 Lock 어노테이션 추가

--- src/main/java/com/ll/netmong/domain/product/repository/ProductRepository.java
+++ src/main/java/com/ll/netmong/domain/product/repository/ProductRepository.java
@@ -2,12 +2,21 @@ package com.ll.netmong.domain.product.repository;
 
 import com.ll.netmong.domain.product.entity.Product;
 import com.ll.netmong.domain.product.util.Category;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategory(Category category);
 
     List<Product> findByProductName(String productName);
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Product s where s.id = :id")
+    Optional<Product> findByIdWithPessimisticLock(@Param("id") Long id);
 }

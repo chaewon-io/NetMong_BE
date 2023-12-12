94744195a26ee7b684bea8fa18fc6e5e4c1b37a3
shinjaewon99 <tlswodnjs99@naver.com>
2023-12-12 13:42:03 +0900

    Feat : 장바구니에 상품 등록기능, ItemCart를 얻는 기능 추가

--- src/main/java/com/ll/netmong/domain/cart/itemCart/service/ItemCartService.java
+++ src/main/java/com/ll/netmong/domain/cart/itemCart/service/ItemCartService.java
@@ -3,10 +3,16 @@ package com.ll.netmong.domain.cart.itemCart.service;
 import com.ll.netmong.domain.cart.dto.request.ProductCountRequest;
 import com.ll.netmong.domain.cart.dto.response.ViewCartResponse;
 import com.ll.netmong.domain.cart.entity.Cart;
+import com.ll.netmong.domain.cart.itemCart.entity.ItemCart;
 
 import java.util.List;
 
 public interface ItemCartService {
-    void addProductByCart(Cart cart, Long productId, ProductCountRequest productCountRequest);
     List<ViewCartResponse> readMemberCartByUser(String findMemberName);
+
+    void addToCartForNewProduct(Cart cart, Long productId, ProductCountRequest productCountRequest);
+
+    void addToCartForExistingProduct(ItemCart findItemCart, Cart cart, ProductCountRequest productCountRequest);
+
+    ItemCart getItemCart(Cart cart, Long productId);
 }
